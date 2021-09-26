1e3d34de3d2f6d9f79b26978bcb26c85e9c301c2
whitehet-sex-lman <91036601+whitehet-sex-lman@users.noreply.github.com>
2021-09-26 21:57:29 +0300

    Update Source.cpp

--- 2021.09.13 Lesson 1/Project10/Source.cpp
+++ 2021.09.13 Lesson 1/Project10/Source.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    int a = 0,b=0;
+    int a = 0,b = 0;
    cin >> a>>b;
     cout << (a * (a / b) + b * (b / a)) / (b / a + a / b) << endl;
     
